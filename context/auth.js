c8f2b115bdd078b041f5f68397174d2685e2e3f5
Sahilbalhra <sahilbalhra0@gmail.com>
2022-06-14 14:12:00 +0530

    some changes

--- context/auth.js
+++ context/auth.js
@@ -1,55 +1,64 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from 'firebase/auth'
-import React, { useEffect } from 'react'
-import { auth } from '../firebase'
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import React, { useEffect } from "react";
+import { auth } from "../firebase";
 
-export const AuthContext = React.createContext()
+export const AuthContext = React.createContext();
 
 function AuthWrapper({ children }) {
-    
-    const [user, setUser] = React.useState('')
-    const [loading,setLoading] = React.useState(true)
+  const [user, setUser] = React.useState("");
+  const [loading, setLoading] = React.useState(true);
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, (user) => {
-            console.log(user)
-            if(user){
-                setUser(user)
-            }else{
-                setUser('')
-            }
-        })
-        setLoading(false);
-    },[])
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (user) {
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+    setLoading(false);
+  }, []);
 
-    function login(email,password){
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  //function to login
+  function login(email, password) {
+    //auth from firebase file
+    //signInWithEmailAndPassword : it is a firebase function to login
+    return signInWithEmailAndPassword(auth, email, password);
+  }
 
-    function logout(){
-        return signOut(auth);
-    }
+  function logout() {
+    return signOut(auth);
+  }
 
-    function forgot(email){
-        return sendPasswordResetEmail(auth, email)
-    }
+  function forgot(email) {
+    return sendPasswordResetEmail(auth, email);
+  }
 
-    function signup(email,password){
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
 
-    const store = {
-        login,
-        user,
-        logout,
-        forgot,
-        signup
-    } 
+  //hold all the function in it of firebase
+  const store = {
+    login,
+    user,
+    logout,
+    forgot,
+    signup,
+  };
 
-    return (
-        <AuthContext.Provider value={store}>
-            {!loading && children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={store}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
 
-export default AuthWrapper
+export default AuthWrapper;
--- firebase.js
+++ firebase.js
@@ -1,4 +1,29 @@
 // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDISoG3WY02ZnyMIz7coHNoynejfG39OTA",
+//   authDomain: "next-reels.firebaseapp.com",
+//   projectId: "next-reels",
+//   storageBucket: "next-reels.appspot.com",
+//   messagingSenderId: "150609612898",
+//   appId: "1:150609612898:web:6689089ef18ed09b2bc13b"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth();
+// const storage = getStorage();
+// export { auth, storage };
+// export default app;
+
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
