9687c899cd64381226bc9dcfc7a9bc26ab32cb3f
Sahilbalhra <sahilbalhra0@gmail.com>
2022-06-14 13:42:08 +0530

    firebase change

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABtHPrPdKMwqNt41ujK_3o-GQl88KdlCM",
-  authDomain: "reels-next-yt-273f7.firebaseapp.com",
-  projectId: "reels-next-yt-273f7",
-  storageBucket: "reels-next-yt-273f7.appspot.com",
-  messagingSenderId: "748556218319",
-  appId: "1:748556218319:web:6ae8c9233226d236822cba",
+  apiKey: "AIzaSyDISoG3WY02ZnyMIz7coHNoynejfG39OTA",
+  authDomain: "next-reels.firebaseapp.com",
+  projectId: "next-reels",
+  storageBucket: "next-reels.appspot.com",
+  messagingSenderId: "150609612898",
+  appId: "1:150609612898:web:6689089ef18ed09b2bc13b"
 };
 
 // Initialize Firebase
